0216fe0f9443b5ebdd3702fbe3ddb3cbcd82bbb6
Kaixiang Zhang <kaixiangzhang@Kaixiangs-MacBook-Pro.local>
2020-01-21 09:00:24 -0700

    upload

---
+++ hello.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import cgi
+import cgitb
+import os
+print("Content-Type: text/html\n")
+print()
+
+#Q1
+#print(os.environ)
+#Q2
+#print(os.environ["QUERY_STRING"])
+#Q3
+#print(os.environ["HTTP_USER_AGENT"])
+#Q4
